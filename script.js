45a6a32fa10335c0a264d99620b56498d0bb7541
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-08-16 18:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// your code here
+let h3 = document.getElementById("heading")
+let nam = document.getElementById("name")
+let year = document.getElementById("year")
+let btn = document.getElementById("btn")
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let x = `?name=${nam.value}&year=${year.value}`
+    let y = `?name=${nam.value}`
+    let z = `?year=${year.value}`
+    if (nam.value && year.value) {
+        h3.textContent = h3.innerText + x;
+    }
+    if (nam.value && !year.value) {
+        h3.textContent = h3.innerText + y
+    }
+    if (!nam.value && year.value) {
+        h3.textContent = h3.innerText + z
+    }
+    nam.value = ' '
+    year.value = ' '
+})
