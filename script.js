c448c750e4bb86bf398cb2daee7d5d7af57a8841
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-08-16 18:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let h3 = document.getElementById("heading")
-let nam = document.getElementById("name")
-let year = document.getElementById("year")
-let btn = document.getElementById("btn")
+let h3 = document.getElementById("url");
+let nam = document.getElementById("name");
+let year = document.getElementById("year");
+let btn = document.getElementById("button");
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    let x = `?name=${nam.value}&year=${year.value}`
-    let y = `?name=${nam.value}`
-    let z = `?year=${year.value}`
+    let x = `?name=${nam.value}&year=${year.value}`;
+    let y = `?name=${nam.value}`;
+    let z = `?year=${year.value}`;
     if (nam.value && year.value) {
         h3.textContent = h3.innerText + x;
     }
     if (nam.value && !year.value) {
-        h3.textContent = h3.innerText + y
+        h3.textContent = h3.innerText + y;
     }
     if (!nam.value && year.value) {
-        h3.textContent = h3.innerText + z
+        h3.textContent = h3.innerText + z;
     }
-    nam.value = ' '
-    year.value = ' '
+    nam.value = ' ';
+    year.value = ' ';
 })
